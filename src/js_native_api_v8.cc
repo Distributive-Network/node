2a4b2f17ef1febdbdffdd35d7d98344a7b9e3e87
Jason Erb <jason.erb@sparist.com>
2023-02-02 22:01:26 -0500

    Remove includes of upward dependencies
    NAPI_EXPERIMENTAL had to be removed because
    it caused a NAPI_VERSION definition conflict.

--- src/js_native_api_v8.cc
+++ src/js_native_api_v8.cc
@@ -1,10 +1,6 @@
 #include <algorithm>
 #include <climits>  // INT_MAX
 #include <cmath>
-#ifndef NAPI_EXPERIMENTAL
-#define NAPI_EXPERIMENTAL
-#endif
-#include "env-inl.h"
 #include "js_native_api.h"
 #include "js_native_api_v8.h"
 #include "util-inl.h"
